64679843b0af71c2d6dfeda915963bdb97355e82
kikito <nikita.tyunyayev@student.uclouvain.be>
2022-10-18 14:57:45 +0000

    changing client and server scripts

--- exec_scripts/newClient.sh
+++ exec_scripts/newClient.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./dpdk_picoquicdemo --dpdk -l 0-1 -a 0000:18:00.0 -- -X -D -A 50:6b:4b:f3:7c:70 -* 32 -@ 32 10.100.0.2 4445 /20000000000
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./dpdk_picoquicdemo --dpdk -l 2-3 -a 0000:18:00.0 -- -X -D -A 50:6b:4b:f3:7c:70 -* 32 -@ 32 10.100.0.2 4443 /20000000000
 #sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./dpdk_picoquicdemo --dpdk -l 0-8 -a 0000:18:00.2 -a 0000:18:00.3 -a 0000:18:00.4 -a 0000:18:00.5 -a 0000:18:00.6 -a 0000:18:00.7 -a 0000:18:01.0 -a 0000:18:01.1 -- -D -A 50:6b:4b:f3:7c:70 -* 32 -@ 4 10.100.0.2 4443 /200
--- exec_scripts/newServer.sh
+++ exec_scripts/newServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./dpdk_picoquicdemo --dpdk -l 0-1 -a 0000:18:00.0 --  -* 32 -@ 32 -p 4443 -1
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./dpdk_picoquicdemo --dpdk -l 2-3 -a 0000:18:00.0 --  -* 32 -@ 32 -p 4443 -1
